ddba2287d62a3427c4464773410173e78a484109
soohyang136 <jsh515228@gmail.com>
2022-06-14 12:47:46 +0900

    단어 유무 검사, 실패 시 리프래쉬

--- src/components/Word.js
+++ src/components/Word.js
@@ -10,11 +10,12 @@ export default function Word(){
     const [count, setCount] = useState(0);
     let definition = '';
     function OnClick(){
-        if(apro === 0 || data[apro-1].wording[data[apro-1].wording.length - 1] === word[0]){
+        if(apro === 0 || data[apro-1].wording[data[apro-1].wording.length - 1] === word[0]) {
             async function search (){
                 const url = "/api/search?certkey_no=3972&key=60C5985FAD0CE908A6FE289E8D2801F4&target_type=search&req_type=json&part=word&q="+ word +"&sort=dict&start=1&num=10";
                 const response = await axios.get(url, );
-                if(response.data){
+                if(response.data.channel.total !== '0') {
+                    console.log(response);
                     setCount(count+1);
                     definition = response.data.channel.item[0].sense[0].definition;
                     data[apro] = {
@@ -23,15 +24,17 @@ export default function Word(){
                     };
                     apro += 1;
                 }
-                else{
+                else {
                     alert("없는 단어 입니다.");
+                    window.location.replace("/");
                     setCount(0);
                 }
             }
             search();
         }
-        else{
+        else {
             alert("실패");
+            window.location.replace("/");
         }
         setWord('')
     };
@@ -40,7 +43,7 @@ export default function Word(){
     };
 
     function onKeyPress(e){
-        if(e.key === "Enter"){
+        if(e.key === "Enter") {
             OnClick();
         }
     }
@@ -49,10 +52,9 @@ export default function Word(){
         <div className="wordchain" onKeyPress={onKeyPress}>
             <input type="text" placeholder="단어입력" value={word} onChange={OnChange}/>
             <button onClick={OnClick}>제출</button>
-            {data.slice(0).reverse().map( (p, key) => {
-                return <Print wording = {p.wording} define = {p.define} key={key}/>
+            {data.slice(0).reverse().map( (p, index) => {
+                return <Print wording = {p.wording} define = {p.define} key={index}/>
             })
-
             }
         </div>
     )
