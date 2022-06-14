c96724ed4c3aed0bb282c061d3ee37ed0bd3c169
soohyang136 <jsh515228@gmail.com>
2022-06-14 17:47:17 +0900

    한글자 검사

--- src/components/Word.js
+++ src/components/Word.js
@@ -11,26 +11,32 @@ export default function Word(){
     let definition = '';
     function OnClick(){
         if(apro === 0 || data[apro-1].wording[data[apro-1].wording.length - 1] === word[0]) {
-            async function search (){
-                const url = "/api/search?certkey_no=3972&key=60C5985FAD0CE908A6FE289E8D2801F4&target_type=search&req_type=json&part=word&q="+ word +"&sort=dict&start=1&num=10";
-                const response = await axios.get(url, );
-                if(response.data.channel.total !== '0') {
-                    console.log(response);
-                    setCount(count+1);
-                    definition = response.data.channel.item[0].sense[0].definition;
-                    data[apro] = {
-                        wording: word,
-                        define: definition,
-                    };
-                    apro += 1;
+            if(word.length > 1){
+                async function search (){
+                    const url = "/api/search?certkey_no=3972&key=60C5985FAD0CE908A6FE289E8D2801F4&target_type=search&req_type=json&part=word&q="+ word +"&sort=dict&start=1&num=10";
+                    const response = await axios.get(url, );
+                    if(response.data.channel.total !== '0') {
+                        console.log(response);
+                        setCount(count+1);
+                        definition = response.data.channel.item[0].sense[0].definition;
+                        data[apro] = {
+                            wording: word,
+                            define: definition,
+                        };
+                        apro += 1;
+                    }
+                    else {
+                        alert("없는 단어 입니다.");
+                        window.location.replace("/");
+                        setCount(0);
+                    }
                 }
-                else {
-                    alert("없는 단어 입니다.");
-                    window.location.replace("/");
-                    setCount(0);
-                }
+                search();
             }
-            search();
+            else{
+                alert("단어는 한글자 이상이여야 합니다!");
+            }
+            
         }
         else {
             alert("실패");
